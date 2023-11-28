6482896362327d8779a19829e480de849d6a5815
HoJun Jang <bigfanoftim@gmail.com>
2023-11-28 11:05:55 +0900

    초기세팅

---
+++ src/main/java/me/bigfanoftim/nested/ExampleNestedTestApplication.java
@@ -0,0 +1,13 @@
+package me.bigfanoftim.nested;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleNestedTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleNestedTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/bigfanoftim/nested/ExampleNestedTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bigfanoftim.nested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleNestedTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
